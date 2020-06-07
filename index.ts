62cc43ef990330e8f95fec1d738f1e3e6e9676d6
mm-masahiro <hand5.mm9.mzh@gmail.com>
2020-06-07 16:12:25 +0900

    TSの基礎学習

--- index.ts
+++ index.ts
@@ -39,3 +39,82 @@ fruits.push('Melon');
 console.log(fruits);
 
 const book: [string, number, boolean] = ['business', 1500, false];
+
+enum coffeSize  {
+  SHORT = 'SHORT',
+  TALL = 'TALL',
+  GRANDE = 'GRANDE',
+  VENTI = 'VENTI'
+}
+
+const coffee = {
+  hot: true,
+  size: coffeSize.TALL
+}
+
+//unionタイプ
+let unionType: number | string = 10;
+unionType = 'hello';
+
+let unionTypes: (number | string)[] = ['hello', 23, 'acnosc'];
+
+
+//リテラル型 =>決まった値のみを取り扱う   constを使うとリテラル型が採用される
+//Typeエイリアス
+type ClothSize = 'small' | 'medium' | 'large'
+const apple: 'apple' = 'apple'
+
+let clothSize: 'small' | 'medium' | 'large' = 'large'
+const cloth: {
+  color: string;
+  size: ClothSize
+}
+ = {
+  color: 'white',
+  size: clothSize
+}
+
+//関数に型をつける
+function Add(num1: number, num2: number): number {
+  return num1 + num2
+}
+
+function sayHello(): void {
+  console.log('Hello');
+}
+
+console.log(sayHello());
+
+const anotherAdd: (n1: number, n2: number) => number = add;
+
+const doubleNumber: (num: number) => number = num => num * 2;
+
+//callback関数の型
+function doubleAndHandle(num: number, cb: (num: number) => number): void {
+  const doubleNum = cb(num * 2)
+  console.log(doubleNum);
+}
+
+doubleAndHandle(2, doubleNum => {
+  return doubleNum
+});
+
+//unknon型　←any型よりも少し厳しいもの
+let unknonInput: unknown;
+let anyInput: any;
+let text: string;
+unknonInput = 'hello';
+unknonInput = 21;
+unknonInput = true;
+text = anyInput;
+//unknown型はなんでも入れることができるが、使うときは注意
+//使うときは以下のようにtypeofで使う型を保証する必要がある
+if (typeof unknonInput === 'string') {
+  text = unknonInput;
+}
+
+//never型
+function error(message: string): never {
+  throw new Error(message);
+}
+console.log(error('This is an error'));
